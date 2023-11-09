9d99eeb91a7cda3a4ddb4978ba71e2835ffe82fa
mechack tambwe <mesutarso@users.noreply.github.com>
2023-11-10 00:30:54 +0100

    add cahe dependacy

---
+++ config/plugins.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  // ..
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+      requestTransforms: {
+        wrapBodyWithDataKey: true,
+      },
+    },
+  },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3600,
+        },
+      },
+      
+    },
+  },
+  // ..
+});
